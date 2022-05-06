2c6d67d22afe47bc99b8abaf00ea809e1a0610b4
Aleksei Novikov <alex.nov@list.ru>
2022-05-06 16:23:49 +0300

    Seminare1

---
+++ Seminare1/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+5. Напишите программу вычисления значения
+функции возведения числа в квадрат.
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.WriteLine("Введите число");
+int namber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(namber * namber);
+Console.w
