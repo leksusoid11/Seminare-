2d866660a5b7fc0390fc60b581bf2d56fc54d51f
Aleksei Novikov <alex.nov@list.ru>
2022-05-06 16:39:19 +0300

    Seminare2

---
+++ Seminare2/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+7. Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+4 -> "-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2 -> " -2, -1, 0, 1, 2"
+*/
+
+Console.WriteLine("Введите число:");
+int namber = Convert.ToInt32(Console.ReadLine());
+for (int i = - namber; i <= namber; i++)
+{
+    Console.Write(i);
+    if (i != namber)
+{
+    Console.Write(", ");
+}
+
+}
